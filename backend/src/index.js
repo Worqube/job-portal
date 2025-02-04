a231ee9be27e765c150357e11eb94b9b9e68b2be
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-04 22:04:51 +0530

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import { Details, NewDetail, NewUser } from "./controllers/Details.js";
+import { Details, NewDetail, NewUser } from "./controllers/details.controller.js";
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT
+
 
 app.use(express.json());
 
