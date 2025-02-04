2b6641bdfc95bb7454cad96f17bee456a439a4de
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-04 20:49:10 +0530

    signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,2 +1,7 @@
 import bcrypt from "bcryptjs";
 import { User } from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+    const { email, password } = req.body;
+}
