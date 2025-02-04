fbaa2298a54269b1862478c357b16095cd59bc2d
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-04 22:19:40 +0530

    jwt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,4 +4,35 @@ import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
     const { email, password } = req.body;
-}
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        if (password.length < 8) {
+            return res.status(400).json({ message: "Password must be at least 8 letters" });
+        }
+
+        let user = await User.findOne({ email: email });
+        if (user) return res.status(400).json({ message: "Email already exist" });
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPW = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            email: email,
+            password: hashedPW,
+        });
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+            res.status(201).send(newUser)
+        } else {
+            res.status(400).json({ message: "Invalid User Data" })
+        }
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import authRoutes from './routes/auth.routes.js'
 import { Details, NewDetail, NewUser } from "./controllers/details.controller.js";
 
 
@@ -14,6 +15,8 @@ app.use(express.json());
 app.post('/users', NewUser);
 app.post('/users/:userId/details', NewDetail);
 app.get('/users/:userId/details', Details)
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is connected. PORT:", PORT);
