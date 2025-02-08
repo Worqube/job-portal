753540d2e2e8562a989bc58c73403a14a43d9ff3
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-08 19:32:10 +0530

    css data

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="items-center">
+      <h1 className="items-center text-3xl text-red-400">App</h1>
+    </div>
+  );
+};
+
+export default App;
