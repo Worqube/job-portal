c72be160c5ddfe65f87ea5c2b0cf23623255a6dc
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-08 20:45:21 +0530

    adding cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Signup from "./pages/Signup";
 const App = () => {
   return (
     <div className="items-center">
+      App
       <Routes>
         <Route path="/signup" element={<Signup />} />
       </Routes>
