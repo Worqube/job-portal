62459ecae5d27944b6d3fffff4f852865b1c749d
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-25 15:07:04 +0530

    commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,6 @@ export const generateToken = (userId, res) => {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'strict',
-        secure: process.env.NODE_ENV !== 'development'
     })
 
     return token;
