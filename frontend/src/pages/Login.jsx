0db9038ca4597a638c2907f8dae362a00156ab7b
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-27 22:23:22 +0530

    login forms font change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import StudentLogin from "./StudentLogin";
-import AdminLogin from "./AdminLogin";
+import StudentLogin from "./components/StudentLogin";
+import AdminLogin from "./components/AdminLogin";
 
 const Login = () => {
   const [lit, setLit] = useState("Student");
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-2xl">
-        <h2 className="text-3xl font-bold text-center text-gray-800">Login</h2>
+      <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-xl">
+        <h2 className="text-2xl font-bold text-center text-gray-800">Login</h2>
         <div className="relative flex space-x-2 border-[3px] border-blue-500 rounded-xl select-none overflow-hidden">
           {/* Left Section (Student) */}
           <div
