f4f7b1f9013908cf40c8bdfdb636a7cdc8f46464
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-28 00:49:12 +0530

    lax

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
         maxAge: 24 * 60 * 60 * 1000, // 1 day
         httpOnly: true, // Prevent client-side access
         secure: false, // Secure in production
-        sameSite: "strict", // Allows cross-site requests
+        sameSite: "lax", // Allows cross-site requests
     });
 
     return token;
