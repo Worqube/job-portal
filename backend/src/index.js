8c6e3d32dc9ca8b7bb63a4c64f2dd02bf06d6304
BhavarthPetare <petarebhavarth@gmail.com>
2025-02-28 00:53:32 +0530

    allow

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(cors({
     origin: 'https://worqube.netlify.app',
     credentials: true,
+    allowedHeaders: {
+        "Access-Control-Allow-Origin": "https://worqube.netlify.app",
+        "Access-Control-Allow-Credentials": true,
+    }
 }))
 
 app.use('/users', userRoutes);
