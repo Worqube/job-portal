4ef20d03ac47124f9f5fd66729665cc4c00d0e03
BhavarthPetare <petarebhavarth@gmail.com>
2025-03-02 16:45:46 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,13 @@ import { Loader } from "lucide-react";
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-  if (isCheckingAuth)
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
