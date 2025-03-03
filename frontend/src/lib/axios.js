d66189138e3998745d5000701afad29f6dd8ec84
BhavarthPetare <petarebhavarth@gmail.com>
2025-03-03 22:16:29 +0530

    set right

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
     baseURL: [
         "https://job-portal-6nsa.onrender.com",
-        "http://localhost:6060",
+        // "http://localhost:6060",
     ],
     withCredentials: true,
 });
