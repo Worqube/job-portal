f8f7e9fb291f2d019609480d7cd55554ba2ba0ad
BhavarthPetare <petarebhavarth@gmail.com>
2025-03-03 21:42:46 +0530

    logout

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -68,10 +68,15 @@ export const useAuthStore = create(
             },
             logout: async () => {
                 set({ isLoggingOut: true });
+                const user = sessionStorage.getItem('user');
                 try {
-                    await axiosInstance.post('/auth/logout');
-                    set({ authUser: null });
-                    toast.success('Logged out successfully!');
+                    if (user) {
+                        sessionStorage.removeItem('user');
+                    } else {
+                        await axiosInstance.post('/auth/logout');
+                        set({ authUser: null });
+                        toast.success('Logged out successfully!');
+                    }
                 } catch (error) {
                     toast.error(error.response.data.message);
                 } finally {
