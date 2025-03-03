f749dce288165a68988149a2a3eea9ff31fd07ff
BhavarthPetare <petarebhavarth@gmail.com>
2025-03-03 21:29:56 +0530

    change

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -13,14 +13,18 @@ export const useAuthStore = create(
             isLoggingOut: false,
 
             checkAuth: async () => {
+                const user = sessionStorage.getItem('user');
+                set({ authUser: JSON.parse(user) });
                 set({ isCheckingAuth: true });
-                try {
-                    const res = await axiosInstance.get('/auth/check', { withCredentials: true });
-                    set({ authUser: res.data });
-                } catch (error) {
-                    set({ authUser: null });
-                } finally {
-                    set({ isCheckingAuth: false });
+                if (!authUser) {
+                    try {
+                        const res = await axiosInstance.get('/auth/check', { withCredentials: true });
+                        set({ authUser: res.data });
+                    } catch (error) {
+                        set({ authUser: null });
+                    } finally {
+                        set({ isCheckingAuth: false });
+                    }
                 }
             },
             signup: async (data) => {
@@ -41,6 +45,7 @@ export const useAuthStore = create(
                 try {
                     const res = await axiosInstance.post('/auth/login', data, { withCredentials: true });
                     set({ authUser: res.data });
+                    sessionStorage.setItem("user", JSON.stringify(res.data));
                     toast.success('Logged in successfully');
                 } catch (error) {
                     set({ authUser: null });
