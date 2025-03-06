87d3666a0869f88f8f32a20765a1648b59ed4263
BhavarthPetare <petarebhavarth@gmail.com>
2025-03-06 11:29:02 +0530

    onrender

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: '*',
+    origin: 'https://worqube.onrender.com',
     credentials: true,
 }))
 
