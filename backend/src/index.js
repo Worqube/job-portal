7666403fed145e24bf12591a2113a527642e4566
BhavarthPetare <petarebhavarth@gmail.com>
2025-03-06 11:26:54 +0530

    all requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,11 +15,7 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: [
-        'https://worqube.onrender.com',
-        'https://worqube.netlify.app',
-        // 'http://localhost:5173',
-    ],
+    origin: '*',
     credentials: true,
 }))
 
