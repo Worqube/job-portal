70cdfed493fac2bf539e370ab45e39dae6db1f0f
BhavarthPetare <petarebhavarth@gmail.com>
2025-03-18 10:53:33 +0530

    changes

--- frontend/src/pages/components/Navbar.jsx
+++ frontend/src/pages/components/Navbar.jsx
@@ -3,7 +3,9 @@ import { useAuthStore } from "../../store/useAuthStore";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
-  return (
+  user = sessionStorage.getItem("user");
+  loggedIn = authUser || user;
+  return { loggedIn } ? (
     <div className="navbar h-fit">
       <nav className="flex justify-between items-center py-4 px-8 bg-white shadow-md">
         <div className="text-xl font-bold">TNP Nexus</div>
@@ -16,6 +18,23 @@ const Navbar = () => {
           </a>
           <a href="#" className="text-gray-600 hover:text-gray-900">
             Companies
+          </a>
+          <a href="#" className="text-gray-600 hover:text-gray-900">
+            About
+          </a>
+        </div>
+        <button className="bg-blue-600 text-white px-4 py-2 rounded">
+          Login
+        </button>
+      </nav>
+    </div>
+  ) : (
+    <div className="navbar h-fit">
+      <nav className="flex justify-between items-center py-4 px-8 bg-white shadow-md">
+        <div className="text-xl font-bold">TNP Nexus</div>
+        <div className="space-x-8">
+          <a href="#" className="text-gray-600 hover:text-gray-900">
+            Home
           </a>
           <a href="#" className="text-gray-600 hover:text-gray-900">
             About
